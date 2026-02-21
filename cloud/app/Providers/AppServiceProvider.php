70e632e93ee06210150ce12dd5901c6e58d6deff
Boyan Balkanski <balkanski@outlook.com>
2026-02-21 17:58:52 +0200

    Fix typo in AppServiceProvider to use app() instead of $app for HTTPS redirection

--- cloud/app/Providers/AppServiceProvider.php
+++ cloud/app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         URL::forceHttps(
-            $app->environment(['production', 'staging'])
+            app()->environment(['production', 'staging'])
         );
     }
 }
